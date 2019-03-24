d9f40f03f6634adfad050922e1cf0a8d924eedcd
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-24 22:07:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
-  end 
+  elsif katz_deli.length > 0 
+    katz_deli.each_with_index do |value, index|
+      puts 
   
     
 end 
